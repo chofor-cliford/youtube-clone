5b1c06c6b2e973dfd7353a4fefd94fe4f605c0f9
Better Futures <betterfuture032@gmail.com>
2023-05-01 07:41:28 -0400

    initial commit

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,47 @@
+import { useState, useEffect } from "react";
+import { Box, Stack, Typography } from "@mui/material";
+
+import { categories } from "../utils/constants";
+
+const Sidebar = ({selectedCategory, setSelectedCategory}) => {
+  return (
+    <Stack
+      direction="row"
+      sx={{
+        overflow: "auto",
+        height: { sx: "auto", md: "95%" },
+        flexDirection: { md: "column" },
+      }}
+    >
+      {categories.map((category) => (
+        <button
+          key={category.name}
+          className="category-btn"
+          onClick = {() => setSelectedCategory(category.name)}
+          style={{
+            background: category.name === selectedCategory && "#fc1503",
+            color: "#fff",
+          }}
+        >
+          <span
+            style={{
+              color: category.name === selectedCategory ? "#ffffff" : "#ff0000",
+              marginRight: "15px",
+            }}
+          >
+            {category.icon}
+          </span>
+          <span
+            style={{
+              opacity: category.name === selectedCategory ? "1" : "0.8",
+            }}
+          >
+            {category.name}
+          </span>
+        </button>
+      ))}
+    </Stack>
+  );
+};
+
+export default Sidebar;
